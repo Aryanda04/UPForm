2649b463a3eff679eb45b225674a378328afd2ec
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-06-27 13:32:30 +0700

    complete navbar

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
+import profilePicture from "../assets/img/amogus-PP.jpg";
+
 
 const Navbar = () => {
   return (
     <>
       <nav>
         <div className="navbar__left">
-          <h1>UPFOrm</h1>
+          <h1>UPForm</h1>
         </div>
         <div className="navbar__right">
-          <h2>test</h2>
+        <a href="">Help</a>
+        <i class="fa fa-bell w3-xlarge" aria-hidden="true"></i>
+          <h2>Your Name</h2>
+          <img src={profilePicture} alt="profile picture" />
         </div>
       </nav>
     </>
