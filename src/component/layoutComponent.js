e9b83b8cbc74b81d2762fe38d799c3edb244b771
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-07-14 22:44:54 +0700

    add sidebar

--- src/component/layoutComponent.js
+++ src/component/layoutComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./navbar";
+import Sidebar from "./sidebar";
 
 const LayoutComponent = (props) => {
   return (
     <>
       <Navbar />
+      <Sidebar/>
       <div>{props.content}</div>
     </>
   );
---
+++ src/component/sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Sidebar = () => {
+  return (
+    <>
+      <nav>
+        this-is sidebar
+      </nav>
+    </>
+  );
+};
+
+export default Sidebar;
